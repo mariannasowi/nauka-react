e8c32b564426334fa6ce3227356190516876f7df
Marianna Sowinska <marianna.sowi@gmail.com>
2020-08-15 15:30:00 +0200

    Module 15.2. Pierwsze kroki w React.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,9 +3,12 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>My first React app</h1>
-      </div>
+      <div id="app">
+        <main className="app-content">
+          <h1>My first React app</h1>
+          <h2>Hello world!</h2>
+        </main>
+      </div> 
     )
   }
 }
