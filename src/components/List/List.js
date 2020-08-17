f0d2650bddda3e1a90b79877d14d132d80fa4d93
Marianna Sowinska <marianna.sowi@gmail.com>
2020-08-17 17:05:16 +0200

    15.5. State – stan komponentu. Wykorzystanie danych z dataStore.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,21 +3,24 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
+import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
   static propTypes = {
     title: PropTypes.node.isRequired,
-    children: PropTypes.node,
+    description: PropTypes.node, 
+    columns: PropTypes.array,
   }
   static defaultProps = {
-    children: <p>I can do all the things!!!</p>,
+    description: <p>I can do all the things!!!</p>,
   }
   render() {
     return (
       <section className={styles.component}>
         <Hero titleText={this.props.title} />
         <div className={styles.description}>
-          {this.props.children}
+          {ReactHtmlParser(this.props.description)}
         </div>
         <div className={styles.columns}>
           <Column title="Animals"/>
