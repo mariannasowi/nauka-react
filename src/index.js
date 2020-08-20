d9b5c39219e1e296ccbd7d7ca619243c65bccc30
Marianna Sowinska <marianna.sowi@gmail.com>
2020-08-20 20:05:41 +0200

    Module 16.3. błędy w consoli, prop type.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import store from './redux/store.js';
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
