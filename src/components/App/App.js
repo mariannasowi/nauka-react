19c8214851f3bbfc807e2dcc5e0568c821d1ba01
Marianna Sowinska <marianna.sowi@gmail.com>
2020-09-09 18:21:50 +0200

    Fix module 17.3. Przejścia pomiędzy widokami.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
+import FAQ from '../FAQ/FAQ';	
+import List from '../List/ListContainer';
 import MainLayout from '../MainLayout/MainLayout';
 import {BrowserRouter, Route} from 'react-router-dom';
 import {AnimatedSwitch} from 'react-router-transition';
@@ -17,6 +19,8 @@ const App = () => (
       >
         <Route exact path="/" component={Home} />
         <Route exact path="/info" component={Info} />
+        <Route exact path='/faq' component={ FAQ } />
+        <Route exact path="/list/:id" component={ List } />
       </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
